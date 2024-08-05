a0a2df2b66630c6ae220cb04bea526250f0fb547
Luciano dos Santos <luciano.satiro1171@gmail.com>
2024-08-05 03:45:17 -0300

    default comment

--- pinguim.sh
+++ pinguim.sh
@@ -5,14 +5,11 @@ R='\033[0m'   # reset
 W='\033[47m'  # branco
 B='\033[40m'  # preto
 Y='\033[43m' # amarelo
-C='\033[46m'   # azul claro
 
 # letra:
 BL='\033[30m'   # preto
-RE='\033[31m'     # vermelho
 GR='\033[32m'   # verde
 YE='\033[33m'  # amarelo
-BLU='\033[34m'    # azul
 MA='\033[35m' # rosa
 CY='\033[36m'    # azul claro
 WH='\033[37m'   # branco
@@ -37,7 +34,7 @@ echo -e "  --------------------------------             "
 echo -e "                                   \           "
 echo -e "                                    \          "
 echo -e "${B}${WH}                                     ${GR}\ ${WH}.--.    "
-echo -e "                                      |o${YE}_${WH}o |   "
+echo -e "                                      |o${BL}_${WH}o |   "
 echo -e "                                      |${Y}${BL}:_/${B}${WH} |   "
 echo -e "                                     //${W}${BL}.--.${B}${WH}${X}  "
 echo -e "                                    (/${W}${BL}(    )${B}${WH}\) "
