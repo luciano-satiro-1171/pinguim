3fbcf601d66e9c29a33905cdbb7f02f15da07a18
luciano_santos <116721746+luciano-satiro-1171@users.noreply.github.com>
2024-09-09 12:09:41 -0300

    Update pinguim.sh

--- pinguim.sh
+++ pinguim.sh
@@ -3,7 +3,7 @@
 # author:      luciano dos santos
 # contact:     https://www.linkedin.com/in/luciano-dos-santos-satiro
 # created:     05/08/2024
-# revision:    15/08/2024
+# revision:    09/09/2024
 # rule 1:      sem acento
 # rule 2:      variaveis em maiusculo
 # rule 3:      funcoes em maiusculo
@@ -13,12 +13,14 @@
 # obs.: as variaveis devem estar com aspas simples
 
 # variaveis:
+LOG="./arq.log"
 X='\\\\'
 # cores de fundo:
 B='\033[40m'  # preto
 W='\033[47m'  # branco
 R='\033[0m'   # reset
 Y='\033[43m'  # amarelo
+
 # cores de letras:
 BL='\033[30m' # preto
 GR='\033[32m' # verde
@@ -26,6 +28,15 @@ WH='\033[37m' # branco
 BO='\033[1m'  # negrito
 
 # funcoes:
+# evita repeticao do if e else:
+IF_ELSE() {
+    if [ $? -eq 0 ]; then
+        echo "[success] $1" >> $LOG
+    else
+        echo "[error] $2" >> $LOG
+        return 1
+    fi
+}
 
 # operacoes:
 #    .--.    
